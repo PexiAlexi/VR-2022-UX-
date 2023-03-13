447aa0927d56363267bf894e2f28814e94be41eb
PexiAlexi <alexi.strandberg@yh.nackademin.se>
2023-03-13 18:47:20 +0100

    Kommit ganska långt

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float timeRemaining = 120.0f; // 2 minutes in seconds
+    public Text timeText;
+    public Button startButton;
+    public Color textColor = Color.white;
+    public Color timeoutColor = Color.red;
+    public GameObject objectToDisable;
+    public AudioSource ticksource;
+    public AudioClip clipsound;
+    public AudioSource alarmsound;
+    public AudioClip alarmclip;
+
+    private bool isRunning = false;
+
+    void Start()
+    {
+        startButton.onClick.AddListener(StartTimer);
+        timeText.color = textColor;
+    }
+
+    void Update()
+    {
+        if (isRunning)
+        {
+            timeRemaining -= Time.deltaTime;
+
+            UpdateTimeText();
+            if (timeRemaining <= 0)
+            {
+                isRunning = false;
+                timeRemaining = 0;
+                timeText.color = timeoutColor;
+                ticksource.Stop();
+                alarmsound.PlayOneShot(alarmclip);
+                if (objectToDisable != null)
+                {
+                    objectToDisable.SetActive(false);
+                }
+               
+            }
+        }
+    }
+
+    void UpdateTimeText()
+    {
+        int minutes = Mathf.FloorToInt(timeRemaining / 60);
+        int seconds = Mathf.FloorToInt(timeRemaining % 60);
+        timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+    void StartTimer()
+    {
+        isRunning = true;
+        startButton.gameObject.SetActive(false);
+        ticksource.Play();
+
+    }
+}
