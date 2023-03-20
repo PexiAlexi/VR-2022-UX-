a4577e64edf2fe103edab2d46dbfcb414356b44b
PexiAlexi <alexi.strandberg@yh.nackademin.se>
2023-03-20 17:59:41 +0100

    Fixade mycket för spelet

---
+++ Assets/Scripts/Quitgame.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quitgame : MonoBehaviour
+{
+    public void Quitgame2()
+    {
+        Application.Quit();
+    }
+
+
+}
